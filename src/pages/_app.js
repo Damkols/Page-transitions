f2cd8e9cdde61ef1bb2634c3486b8e624611084c
Damkols <damolausmankolapo@gmail.com>
2024-08-29 13:52:59 +0100

    feat: page-transition 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import "@/styles/styles.scss";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, router, pageProps }) {
+  return (
+    <div className="main">
+      <AnimatePresence>
+        <Component key={router.route} {...pageProps} />
+      </AnimatePresence>
+    </div>
+  );
 }
---
+++ src/pages/about/index.jsx
@@ -0,0 +1,27 @@
+import Inner from "@/components/Layout/Inner";
+
+export default function About() {
+  return (
+    <>
+      <Inner>
+        <h1>About</h1>
+        <div className="body">
+          <p>
+            {" "}
+            Nullam mattis mattis dictum. Praesent sit amet condimentum mi, quis
+            venenatis lectus. Phasellus ac ante id purus viverra hendrerit quis
+            at ex. Donec vitae augue pulvinar augue dictum fermentum dapibus nec
+            tellus.
+          </p>
+          <p>
+            Phasellus cursus, ante in eleifend vehicula, leo metus mattis est,
+            sit amet dignissim dui nibh in dui. Nullam dictum tortor vitae quam
+            condimentum, eget tincidunt nisi tincidunt. Praesent ut erat at
+            purus gravida aliquam non a purus. Pellentesque sagittis in justo
+            vel venenatis.
+          </p>
+        </div>
+      </Inner>
+    </>
+  );
+}
